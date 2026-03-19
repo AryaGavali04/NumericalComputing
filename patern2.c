f5711723a567f8237ab021fb9ae9087d4bd303b2
AryaGavali <aryapgavali@gmail.com>
2026-03-19 13:15:42 +0530

    Second day patern program

---
+++ patern2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the Number: \n");
+    scanf("%d",&n);
+
+    for(int i=0;i<n;++i){
+        // /for(int j=0;j>i-1;j++){
+        printf("#");
+printf("\n");
+    }
+    // printf("\n");
+// }
+    return 0;
+}
